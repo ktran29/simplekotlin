7490703fd480eb1a571f6b424cd811df0b466732
Kevin Tran <ktran@tack.tech>
2018-04-04 14:58:35 -0700

    remove comments

--- main.kts
+++ main.kts
@@ -99,16 +99,6 @@ class Money(var amount: Int, var currency: String) {
 
 }
 
-// write a "whenFn" that takes an arg of type "Any" and returns a String
-
-// write an "add" function that takes two Ints, returns an Int, and adds the values
-// write a "sub" function that takes two Ints, returns an Int, and subtracts the values
-// write a "mathOp" function that takes two Ints and a function (that takes two Ints and returns an Int), returns an Int, and applies the passed-in-function to the arguments
-
-// write a class "Person" with first name, last name and age
-
-// write a class "Money"
-
 // ============ DO NOT EDIT BELOW THIS LINE =============
 
 print("When tests: ")
